173af129911b6bdf022cb0068ad77b494d919df3
vsheo <vireshsheoratan@gmail.com>
2025-10-17 11:43:38 +0200

    feat: about us #59

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,18 @@
+<section>
+    <h2>About us</h2>
+    <p>Designing for Recognition is an interdisciplinary research group at the Amsterdam University of Applied Sciences. Together with students, designers, technology developers, policymakers, (local) governments, academic researchers, and citizens, we explore how design & technology can contribute to civic life: the numerous and varied interactions through which people in a society come together to strive towards collective well-being</p>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: var(--color-primary);
+    }
+
+    p {
+        width: clamp(25ch, 90%, 85ch);
+        line-height: 1.5rem;
+    }
+</style>
