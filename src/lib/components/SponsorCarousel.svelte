43dd10771ef150cb918e9873ba1c0aafeb0442ee
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-10-27 00:21:33 +0100

    Sponsor carousel HTML (#45)

---
+++ src/lib/components/SponsorCarousel.svelte
@@ -0,0 +1,43 @@
+<section class="sponsors">
+    <header class="title">
+    <h2>Onze Partners</h2>
+    </header>
+
+    <!-- Bovenste rij (links → rechts) -->
+    <div class="logo-top">
+    <ul class="logo-strip-top">
+        <li><img src="/assets/AMS_LogoWHITE.png" alt="AMS Institute"></li>
+        <li><img src="/assets/City-of-Amsterdam.png" alt="City of Amsterdam"></li>
+        <li><img src="/assets/City-of-Rotterdam.png" alt="Gemeente Rotterdam"></li>
+        <li><img src="/assets/TUDelft.png" alt="TU Delft"></li>
+        <li><img src="/assets/Waag.png" alt="Waag Futurelab"></li>
+    </ul>
+    <ul class="logo-strip-top">
+        <li><img src="/assets/AMS_LogoWHITE.png" alt="AMS Institute"></li>
+        <li><img src="/assets/City-of-Amsterdam.png" alt="City of Amsterdam"></li>
+        <li><img src="/assets/City-of-Rotterdam.png" alt="Gemeente Rotterdam"></li>
+        <li><img src="/assets/TUDelft.png" alt="TU Delft"></li>
+        <li><img src="/assets/Waag.png" alt="Waag Futurelab"></li>
+    </ul>
+    </div>
+
+    <!-- Onderste rij (rechts → links, omgekeerde volgorde) -->
+    <div class="logo-bottom">
+    <ul class="logo-strip-bottom">
+        <li><img src="/assets/Waag.png" alt="Waag Futurelab"></li>
+        <li><img src="/assets/TUDelft.png" alt="TU Delft"></li>
+        <li><img src="/assets/City-of-Rotterdam.png" alt="Gemeente Rotterdam"></li>
+        <li><img src="/assets/City-of-Amsterdam.png" alt="City of Amsterdam"></li>
+        <li><img src="/assets/AMS_LogoWHITE.png" alt="AMS Institute"></li>
+    </ul>
+    <ul class="logo-strip-bottom">
+        <li><img src="/assets/Waag.png" alt="Waag Futurelab"></li>
+        <li><img src="/assets/TUDelft.png" alt="TU Delft"></li>
+        <li><img src="/assets/City-of-Rotterdam.png" alt="Gemeente Rotterdam"></li>
+        <li><img src="/assets/City-of-Amsterdam.png" alt="City of Amsterdam"></li>
+        <li><img src="/assets/AMS_LogoWHITE.png" alt="AMS Institute"></li>
+    </ul>
+    </div>
+</section>
+
+</style>
