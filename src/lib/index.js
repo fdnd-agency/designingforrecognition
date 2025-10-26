2c46e638ae55570dcd004f7c51773cc91df554b0
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-10-27 00:24:13 +0100

    Sponsor images (#45)

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 // Components
 export { default as TestLinks } from './components/Test.svelte'
+
+export { default as SponsorCarousel } from './components/SponsorCarousel.svelte'
+
