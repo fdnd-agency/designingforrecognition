1c13140384c6a6d2c27182b541539997632821b3
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-11-05 12:41:19 +0100

    Chore: prepare layout for upcoming header component (#74)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	// dit is voorbeeld voor header dus vervang SponsorCarousel met de header component
+	import {  } from '$lib'
+
 </script>
 
 <svelte:head>
@@ -9,4 +13,11 @@
 	<link rel="stylesheet" href="styles/styleguide.css">
 </svelte:head>
 
+<!-- header component moet hier komen -->
+<!-- <SponsorCarousel/> -->
+
 {@render children?.()}
+
+
+<!-- hier kan je de styling zetten -->
+<style></style>
