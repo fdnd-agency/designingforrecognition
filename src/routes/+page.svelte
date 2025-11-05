0cdf032c105dcd5f0069a33cfcbca76067b58337
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-11-05 12:45:25 +0100

    Style: restructure page layout and add spacing (#74)
    Updated the page structure with grid layout for projects and added margin below the about section for better spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,45 @@
 <script>
-    import { SponsorCarousel, ContainerProjectCard } from '$lib'
+  // componenten importeren zet na SponsorCarousel een "," dan kan je alles erin doen
+    import { ContainerProjectCard, About, FooterInfo, ProjectCard, SponsorCarousel  } from '$lib'
     export let data
+    console.log(data.projects)
 </script>
 
-<ContainerProjectCard {data}/>
+<main>
+<!-- alle componenten kunnen we hier inladen --> 
+<ContainerProjectCard data={data.projects}/>
+<section class="projects-grid">
+{#each data.projects.slice(0,4) as project}
+<ProjectCard project={data.projects[0]}/>
+{/each}</section>
+<section class="about-us">
+<About/></section>
+<SponsorCarousel data={data.sponsors}/>
 
-<SponsorCarousel {data}/>
+
+
+
+
+</main>
+
+<FooterInfo/>
+
+
+<style>
+
+.projects-grid {
+  display:grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, auto);
+  gap:1.5rem;
+}
+
+    :global(body) {
+        margin: 0;
+    }
+
+    .about-us {
+  margin-bottom: 2rem; /* of 60px, pas aan naar smaak */
+}
+    
+</style>
