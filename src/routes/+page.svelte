daf4d01fcbba4afeffbafd017927b9f090392568
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-11-11 21:43:02 +0100

    refactor: simplify main layout and disable unused project grid styling (#74) (#86)
    Removed unused components and commented out .projects-grid styling for cleaner layout structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,30 @@
 <script>
   // componenten importeren zet na SponsorCarousel een "," dan kan je alles erin doen
-    import { ContainerProjectCard, About, FooterInfo, ProjectCard, SponsorCarousel  } from '$lib'
+    import { ContainerProjectCard, About, SponsorCarousel  } from '$lib'
     export let data
     console.log(data.projects)
 </script>
 
 <main>
-<!-- alle componenten kunnen we hier inladen --> 
-<ContainerProjectCard data={data.projects}/>
-<section class="projects-grid">
-{#each data.projects.slice(0,4) as project}
-<ProjectCard project={data.projects[0]}/>
-{/each}</section>
-<section class="about-us">
-<About/></section>
-<SponsorCarousel data={data.sponsors}/>
+	<ContainerProjectCard {data}/>
+	<About />
+	<SponsorCarousel {data} />
+</main> 
 
 
-
-
-
-</main>
-
-<FooterInfo/>
 
 
 <style>
 
-.projects-grid {
+/* .projects-grid {
   display:grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, auto);
   gap:1.5rem;
-}
+} */
 
     :global(body) {
         margin: 0;
     }
-
-    .about-us {
-  margin-bottom: 2rem; /* of 60px, pas aan naar smaak */
-}
     
 </style>
