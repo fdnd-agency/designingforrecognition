85810d7e3e93e45c501d43ff80632500b07ed15a
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2025-11-11 16:55:17 +0100

    #48 #57 Added styling with input checked
    i added a checked in css so when the user chooses a filter, it has a bit of 3d active stylish look.

--- src/lib/components/Filters.svelte
+++ src/lib/components/Filters.svelte
@@ -5,22 +5,22 @@
 <section class="filters">
   <h3>Filteren</h3>
   <div class="checkbox-button">
-    <input type="checkbox" id="filter-ideeën" name="projects" value="Ideeën" />
+    <input type="checkbox" id="filter-ideeën" name="projects" value="Ideeën" alt="filter op Ideeën" />
     <label for="filter-ideeën">Ideeën</label>
   </div>
 
   <div class="checkbox-button">
-    <input type="checkbox" id="filter-participatie" name="projects" value="Participatie" />
+    <input type="checkbox" id="filter-participatie" name="projects" value="Participatie" alt="filter op Participatie" />
     <label for="filter-participatie">Participatie</label>
   </div>
 
   <div class="checkbox-button">
-    <input type="checkbox" id="filter-experimenten" name="projects" value="Experimenten" />
+    <input type="checkbox" id="filter-experimenten" name="projects" value="Experimenten" alt="filter op Experimenten" />
     <label for="filter-experimenten">Experimenten</label>
   </div> 
 
   <div class="checkbox-button">
-    <input type="checkbox" id="filter-methodes" name="projects" value="Methodes" />
+    <input type="checkbox" id="filter-methodes" name="projects" value="Methodes" alt="filter op Methodes" />
     <label for="filter-methodes">Methodes</label>
   </div> 
 </section>
@@ -69,6 +69,11 @@
   .checkbox-button input:checked + label {
     background: var( --color-accent-primary);
     border-color: var( --color-accent-primary);
+    transition: ease-in 0.3s;
+  }
+
+  .checkbox-button input:checked + label {
+    box-shadow:0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2), 0 5px 10px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.2), 0 20px 20px rgba(0, 0, 0, 0.15); 
   }
 
   @media  (max-width: 320px) {
