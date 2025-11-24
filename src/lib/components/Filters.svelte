80e47aede28bbb847b8611af6fcc9fa418e28ed6
vsheo <vireshsheoratan@gmail.com>
2025-11-24 14:21:47 +0100

    filter component in dev layout

--- src/lib/components/Filters.svelte
+++ src/lib/components/Filters.svelte
@@ -2,111 +2,115 @@
 </script>
 
 <form action="/" method="post">
-  <h3>Filteren</h3>
-  <div class="checkbox-button">
-    <input type="checkbox" id="filter-ideeën" name="projects" value="Ideeën" alt="filter op Ideeën" />
-    <label for="filter-ideeën">Ideeën</label>
-  </div>
+	<h3>Filteren</h3>
+	<div class="checkbox-button">
+		<input type="checkbox" id="filter-ideeën" name="projects" value="Ideeën" alt="filter op Ideeën" />
+		<label for="filter-ideeën">Ideeën</label>
+	</div>
 
-  <div class="checkbox-button">
-    <input type="checkbox" id="filter-participatie" name="projects" value="Participatie" alt="filter op Participatie" />
-    <label for="filter-participatie">Participatie</label>
-  </div>
+	<div class="checkbox-button">
+		<input type="checkbox" id="filter-participatie" name="projects" value="Participatie" alt="filter op Participatie" />
+		<label for="filter-participatie">Participatie</label>
+	</div>
 
-  <div class="checkbox-button">
-    <input type="checkbox" id="filter-experimenten" name="projects" value="Experimenten" alt="filter op Experimenten" />
-    <label for="filter-experimenten">Experimenten</label>
-  </div> 
+	<div class="checkbox-button">
+		<input type="checkbox" id="filter-experimenten" name="projects" value="Experimenten" alt="filter op Experimenten" />
+		<label for="filter-experimenten">Experimenten</label>
+	</div>
 
-  <div class="checkbox-button">
-    <input type="checkbox" id="filter-methodes" name="projects" value="Methodes" alt="filter op Methodes" />
-    <label for="filter-methodes">Methodes</label>
-  </div> 
+	<div class="checkbox-button">
+		<input type="checkbox" id="filter-methodes" name="projects" value="Methodes" alt="filter op Methodes" />
+		<label for="filter-methodes">Methodes</label>
+	</div>
 </form>
 
 <style>
+	form {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		margin-top: 5em;
+	}
 
-  h3 {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    font-size: var(--font-size-title-section);
-  }
-  form {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin-top: 12px;
-  }
-   .checkbox-button{
-    display: inline-flex;
-    align-items: center;
-    margin: 8px;
-    font-size: var(--font-size-title-paragraph);
-    text-align: center;
-  }
+	h3 {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		font-size: var(--font-size-title-section);
+	}
 
-  .checkbox-button input {
-    position: absolute;
-    opacity: 0;
-    pointer-events: none;
-  }
+	.checkbox-button {
+		display: inline-flex;
+		align-items: center;
+		margin: 8px;
+		font-size: var(--font-size-title-paragraph);
+		text-align: center;
+	}
 
-  .checkbox-button label {
-    display: inline-block;
-    padding: 6px 10px;
-    border: 1px solid var(--color-accent-primary);
-    border-radius: 7.5px;
-    background: var(--color-accent-secondary);
-    border-color: var(--color-accent-secondary);
-    cursor: pointer;
-    user-select: none;
-  }
+	.checkbox-button input {
+		position: absolute;
+		opacity: 0;
+		pointer-events: none;
+	}
 
-  .checkbox-button input:hover + label {
-    background: var( --color-accent-primary);
-    border-color: var( --color-accent-primary);
-    transition: ease-in 0.3s;
-  }
+	.checkbox-button label {
+		display: inline-block;
+		padding: 6px 10px;
+		border: 1px solid var(--color-accent-primary);
+		border-radius: 7.5px;
+		background: var(--color-accent-secondary);
+		border-color: var(--color-accent-secondary);
+		cursor: pointer;
+		user-select: none;
+	}
 
-  .checkbox-button input:checked + label {
-    background: var( --color-accent-primary);
-    border-color: var( --color-accent-primary);
-    transition: ease-out 0.3s;
-    box-shadow:0 1px 0 #ccc, 0 2px 0 #c9c9c9, 
-    0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 
-    0 6px 1px rgb(0, 0, 0, 0.1), 
-    0 0 5px rgb(0, 0, 0, 0.1), 
-    0 1px 3px rgb(0, 0, 0, 0.3), 
-    0 3px 5px rgb(0, 0, 0, 0.2), 
-    0 5px 10px rgb(0, 0, 0, 0.25), 
-    0 10px 10px rgb(0, 0, 0, 0.2), 
-    0 20px 20px rgb(0, 0, 0, 0.15); 
-  }
+	.checkbox-button input:hover + label {
+		background: var(--color-accent-primary);
+		border-color: var(--color-accent-primary);
+		transition: ease-in 0.3s;
+	}
 
-  @media  (max-width: 320px) {
-    form {
-      flex-direction: column;
-    }
-    label:nth-of-type(1) {
-      margin-bottom: 8px;
-    }
-    .checkbox-button {
-      display: flex;
-      justify-content: center;
-    }
-  }
+	.checkbox-button input:checked + label {
+		background: var(--color-accent-primary);
+		border-color: var(--color-accent-primary);
+		transition: ease-out 0.3s;
+		box-shadow:
+			0 1px 0 #ccc,
+			0 2px 0 #c9c9c9,
+			0 3px 0 #bbb,
+			0 4px 0 #b9b9b9,
+			0 5px 0 #aaa,
+			0 6px 1px rgb(0, 0, 0, 0.1),
+			0 0 5px rgb(0, 0, 0, 0.1),
+			0 1px 3px rgb(0, 0, 0, 0.3),
+			0 3px 5px rgb(0, 0, 0, 0.2),
+			0 5px 10px rgb(0, 0, 0, 0.25),
+			0 10px 10px rgb(0, 0, 0, 0.2),
+			0 20px 20px rgb(0, 0, 0, 0.15);
+	}
 
-  @media (min-width: 321px)  {
-    form {
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-    .checkbox-button {
-      display: flex;
-      justify-content: center;
-    }
-  }
+	@media (max-width: 320px) {
+		form {
+			flex-direction: column;
+		}
+		label:nth-of-type(1) {
+			margin-bottom: 8px;
+		}
+		.checkbox-button {
+			display: flex;
+			justify-content: center;
+		}
+	}
 
+	@media (min-width: 321px) {
+		form {
+			flex-direction: row;
+			flex-wrap: wrap;
+			justify-content: center;
+		}
+		.checkbox-button {
+			display: flex;
+			justify-content: center;
+		}
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	// componenten importeren zet na SponsorCarousel een "," dan kan je alles erin doen
-	import { ProjectCardContainer, About, SponsorCarousel } from '$lib'
+	import { Filters, ProjectCardContainer, About, SponsorCarousel } from '$lib'
 	export let data
 	const projectsData = data.projects
 	const sponsorsData = data.sponsors
 </script>
 
 <main>
+	<Filters />
 	<ProjectCardContainer {projectsData} />
 	<About />
 	<SponsorCarousel {sponsorsData} />
