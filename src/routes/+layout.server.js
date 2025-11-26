2f8e9938f9bff7bb28f69be7c7409457b08e63da
vsheo <vireshsheoratan@gmail.com>
2025-11-26 12:17:23 +0100

    refactor: moved sponsor fetch to layout.server.js #58

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,9 +1,17 @@
 export async function load() {
-	const projectsResponse = await fetch('https://fdnd-agency.directus.app/items/dfr_projects');
-
-	const projectsData = await projectsResponse.json();
-
+	const [projectsResponse, sponsorsResponse] = await Promise.all([
+		fetch('https://fdnd-agency.directus.app/items/dfr_projects'),
+		fetch('https://fdnd-agency.directus.app/items/dfr_sponsors')
+	]);
+	
+	const [projectsData, sponsorsData] = await Promise.all([
+		projectsResponse.json(),
+		sponsorsResponse.json()
+	]);
+	
 	return {
-		projects: projectsData.data
+		projects: projectsData.data,
+		sponsors: sponsorsData.data
 	};
 }
+  
--- src/routes/+page.server.js
+++
@@ -1,8 +0,0 @@
-export async function load({ url }) {
-	const SponsorDataResponse = await fetch(`https://fdnd-agency.directus.app/items/dfr_sponsors/`);
-	const Sponsor = await SponsorDataResponse.json();
-
-	console.log(Sponsor)
-
-	return { sponsors: Sponsor.data };
-}
