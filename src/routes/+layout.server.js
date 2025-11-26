fadb45e2d280a7d00e97620c41c4e626f20b1fbc
vsheo <vireshsheoratan@gmail.com>
2025-11-26 12:12:43 +0100

    refactor: removed double sponsor fetch #58

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,17 +1,9 @@
 export async function load() {
-	const [projectsResponse, sponsorsResponse] = await Promise.all([
-		fetch('https://fdnd-agency.directus.app/items/dfr_projects'),
-		fetch('https://fdnd-agency.directus.app/items/dfr_sponsors')
-	]);
-	
-	const [projectsData, sponsorsData] = await Promise.all([
-		projectsResponse.json(),
-		sponsorsResponse.json()
-	]);
-	
+	const projectsResponse = await fetch('https://fdnd-agency.directus.app/items/dfr_projects');
+
+	const projectsData = await projectsResponse.json();
+
 	return {
-		projects: projectsData.data,
-		sponsors: sponsorsData.data
+		projects: projectsData.data
 	};
 }
-  
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,8 @@
-export async function load ({url}) {
-    const SponsorDataResponse = await fetch(`https://fdnd-agency.directus.app/items/dfr_sponsors/`)
-    const Sponsor = await SponsorDataResponse.json()
+export async function load({ url }) {
+	const SponsorDataResponse = await fetch(`https://fdnd-agency.directus.app/items/dfr_sponsors/`);
+	const Sponsor = await SponsorDataResponse.json();
 
-    return {Sponsor: Sponsor.data}
+	console.log(Sponsor)
+
+	return { sponsors: Sponsor.data };
 }
