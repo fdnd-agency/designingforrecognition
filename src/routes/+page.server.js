3205c75fb49992e8c425b12b52b2416b96a9b676
vsheo <vireshsheoratan@gmail.com>
2025-11-27 21:58:20 +0100

    refactor: remove old code & comments #58

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,86 +5,11 @@ export async function load({ url, parent }) {
 	// get the selected filter from form
 	const selectedCategories = url.searchParams.getAll('projects');
 
-	// console.log(projects);
-	console.log(selectedCategories);
-
 	let filteredProjects;
 
-	// // check if there are filters selected
-	// if (selectedCategories.length > 0) {
-	//     // gp through each JavaScript object
-	// 	filteredProjects = projects.filter((project) => {
-	//         // for each active filter, and check per project all key value pairs to see if the filter is inside of one of those values
-	// 		return selectedCategories.some((filter) => {
-	//             // https://www.geeksforgeeks.org/javascript/how-to-iterate-json-object-in-javascript/
-	// 			return Object.entries(project).some(([key, value]) => {
-	// 				// if value is empty, skip it
-	// 				if (!value) return false;
-
-	// 				// If the field is a string
-	// 				if (typeof value === 'string') {
-	//                     // Check if the string contains the filter
-	// 					return value.includes(filter);
-	// 				}
-
-	// 				// If the field is an array
-	// 				if (Array.isArray(value)) {
-	//                     // Check if any item in the array contains the filter
-	// 					return value.some((item) => item.includes(filter));
-	// 				}
-
-	// 				return false;
-	// 			});
-	// 		});
-	// 	});
-	// // If there are no filters return all projects
-	// } else {
-	// 	filteredProjects = projects;
-	// }
-
-	// // check if there are filters selected
-	// if (selectedCategories.length > 0) {
-	// 	// loop door alle mogelijke velden waarin de filter kan staan
-
-	// 	// gebruik de filter op elk van die JSON objecten
-	// 	selectedCategories.forEach((filter) => {
-	// 		// loop through the value van de JSON onject om te kijken als de filter voorkomt
-	// 		filteredProjects = projects.filter((project) =>
-	// 			// project.Participation_level.includes(filter)
-	// 			forEachField(project, filter)
-	// 			// console.log(project)
-	// 		);
-	// 		console.log(filteredProjects)
-	// 	});
-	// 	// if there is no filter, show everything
-	// } else {
-	// 	filteredProjects = projects;
-	// }
-
-	// function forEachField(projectFields, filter) {
-	// 	// https://www.geeksforgeeks.org/javascript/how-to-iterate-json-object-in-javascript/
-	// 	Object.entries(projectFields).forEach(([key, value]) => {
-	// 		if (Array.isArray(value)) {
-	// 			value.forEach(item => {
-	// 				return item.includes(filter)
-	// 			})
-	// 		} else if (typeof value === 'string') {
-	// 			return value.includes(filter);
-	// 		} else {
-	// 			return false
-	// 		}
-	// 	});
-	// 		// console.log(projectFields)
-	// 		console.log(filter)
-	// }
-
-	// check if there are filters selected
 	if (selectedCategories.length > 0) {
-		// loop door alle mogelijke velden waarin de filter kan staan
-
-		// ga per filter door alle JSON objecten
+		// ga per filter door alle JSON objecten. gebruik de functie searchValues om om elke value te controleren als de filter daarin voorkomt
 		selectedCategories.forEach((filter) => {
-			// gebruik de functie searchValues om om elke value te controleren als de filter daarin voorkomt
 			filteredProjects = searchValues(projects, filter);
 		});
 		// if there is no filter, show everything
@@ -92,10 +17,10 @@ export async function load({ url, parent }) {
 		filteredProjects = projects;
 	}
 
+	// This function goes through all the values of the JSON we send to th home page (data for the project cards)
+	// and searches for the filter the user selected
 	function searchValues(collection, filter) {
-
 		return collection.filter((project) => {
-		// https://www.geeksforgeeks.org/javascript/how-to-iterate-json-object-in-javascript/
 			return Object.entries(project).some(([key, value]) => {
 				if (value == null) {
 					return false;
@@ -106,14 +31,11 @@ export async function load({ url, parent }) {
 						typeof item === 'string' && item.includes(filter)
 					);
 				} else {
-					// Alles wat geen string/array is negeren
 					return false;
 				}
 			});
 		});
 	}
-
-	// console.log(filteredProjects);
 
 	return { projects: filteredProjects };
 }
