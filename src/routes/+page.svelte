ac49b8610852175143b1e078ea1c26a66bbb9cef
Mikiyas-hs <mikiyashadis160@gmail.com>
2025-11-27 01:02:36 +0100

    refactor: remove unused sponsors data and cleanup imports (#116)
    Removes the unused sponsorsData variable and SponsorCarousel import from the
    homepage, and cleans up unused component references.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script>
-	// componenten importeren zet na SponsorCarousel een "," dan kan je alles erin doen
-	import { Filters, ProjectCardContainer, About, SponsorCarousel } from '$lib'
+	import { Filters, ProjectCardContainer, About } from '$lib'
 	export let data
 	const projectsData = data.projects
-	const sponsorsData = data.sponsors
 </script>
 
 <main>
 	<Filters />
 	<ProjectCardContainer {projectsData} />
 	<About />
-	<SponsorCarousel {sponsorsData} />
 </main>
