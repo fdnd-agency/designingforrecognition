425d1d68a0d3275ae76bb7b9da93d9132b771813
vsheo <vireshsheoratan@gmail.com>
2025-12-22 18:27:18 +0100

    feat: responsive logo #153

--- src/lib/atoms/Logo.svelte
+++ src/lib/atoms/Logo.svelte
@@ -1,12 +1,14 @@
-<a href="/" class="logo" aria-label="Civic Interaction Design">
-	<span class="logo-text">Designing for Recognition</span>
+<a href="/" aria-label="Designing for recognition">
+	<span class="logo-text">
+		<span>Designing</span>
+		<span>for</span>
+		<span>Recognition</span>
+	</span>
 </a>
 
 <style>
-	.logo {
+	a {
 		text-decoration: none;
-		display: inline-block;
-		margin: 0 15px 0 0;
 	}
 
 	.logo-text {
@@ -17,11 +19,33 @@
 		background-clip: text;
 		-webkit-text-fill-color: transparent;
 
-		font-size: clamp(0.95rem, 2.6vw, 1.7rem);
+		font-size: clamp(0.95rem, calc(5vw + 1rem), 1.7rem);
 		line-height: 1;
-		white-space: nowrap;
+		/* white-space: nowrap; */
 		overflow: hidden;
 		text-overflow: ellipsis;
 		align-self: center;
+	}
+
+	.logo-text {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 0.25em;
+		font-weight: 700;
+		text-transform: uppercase;
+		background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(13, 56, 79, 1) 44%, rgba(139, 203, 189, 1) 100%);
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+
+		font-size: clamp(1rem, calc(4vw + 1rem), 2.5rem);
+		line-height: 1.1;
+	}
+
+	@media (min-width: 680px) {
+		.logo-text {
+			flex-direction: row;
+		}
 	}
 </style>
