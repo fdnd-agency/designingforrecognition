6fbb50d980277d38a7e0f18f1eb50f52a2bd06c3
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-19 00:58:29 +0100

    x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
 	let projects = $state(data.projects)
 </script>
 
-<Filters projectCount={projects.length} />
-<ProjectCardContainer projectsData={data.projects} />
-<About />
+<h3>Welcome to Designing for Recognition</h3>
+    import { SponsorCarousel, ContainerProjectCard } from '$lib'
+    export let data
+</script>
+
+<ContainerProjectCard {data}/>
+
+<SponsorCarousel {data}/>
