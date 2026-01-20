48dcadbb7105b3f771c8878ef7b1be1cbdf86a3d
vsheo <vireshsheoratan@gmail.com>
2026-01-20 13:37:00 +0100

    fix: nav reduced motion #177

--- src/lib/organisms/Header.svelte
+++ src/lib/organisms/Header.svelte
@@ -102,6 +102,10 @@
 		transition:
 			transform 0.25s ease-out,
 			opacity 0.2s ease-out;
+
+		@media (prefers-reduced-motion: reduce) {
+			transition: none;
+		}
 	}
 
 	.open-menu-icon {
@@ -132,6 +136,13 @@
 		transition: transform 0.45s ease-out;
 	}
 
+	@media (prefers-reduced-motion: reduce) {
+		nav,
+		.close-nav {
+			transition: none;
+		}
+	}
+
 	ul {
 		display: flex;
 		flex-direction: column;
@@ -153,6 +164,12 @@
 			&:hover {
 				background-size: 100% 2px;
 			}
+		}
+	}
+
+	@media (prefers-reduced-motion: reduce) {
+		ul a {
+			transition: none;
 		}
 	}
 
